956094c11eec8aecb4d6d68b75197ff6d78bfe83
sol2kwon <amorfatisole@gmail.com>
2022-05-16 19:30:40 +0900

    2022-05-16 age2

--- src/main/java/streams/PersonStream.java
+++ src/main/java/streams/PersonStream.java
@@ -32,9 +32,11 @@ public class PersonStream {
 
         public String toString(){
             int age = Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date())) - (1900+Integer.parseInt(ssn.substring(0,2)));
+            int age1 = Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date())) - (2000+Integer.parseInt(ssn.substring(0,2)));
             String gender = ssn.substring(7).equals("1")||ssn.substring(7).equals("3")?"남자":"여자";
+            int age2 = ssn.substring(7).equals("1")||ssn.substring(7).equals("2")?age:age1;
 
-            return String.format("이름 %s,성별 %s 나이 %s ", name, gender,age);
+            return String.format("이름 %s,성별 %s 나이 %s ", name, gender,age2);
         }
     }
 
@@ -50,7 +52,7 @@ public class PersonStream {
         public List<Person> search(List<Person> arr) {
             return arr
                     .stream()
-                    .filter(e->e.getName().equals("홍길동"))
+                    .filter(e->e.getName().equals("유관순"))
                     .collect(Collectors.toList());
         }
     }
@@ -60,7 +62,7 @@ public class PersonStream {
         List<Person> arr = Arrays.asList(
            Person.builder().name("홍길동").ssn("900120-1").build(),
                 Person.builder().name("김유신").ssn("970620-1").build(),
-                Person.builder().name("유관순").ssn("940928-4").build()
+                Person.builder().name("유관순").ssn("000928-4").build()
         );
         new PersonServiceImpl()
                 .search(arr)
