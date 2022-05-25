c6b179413bf275010cb9564b854f99b824c23f09
sol2kwon <amorfatisole@gmail.com>
2022-05-25 12:21:21 +0900

    20220525

--- src/main/java/com/toomuchcoder/api/auth/domain/Messenger.java
+++ src/main/java/com/toomuchcoder/api/auth/domain/Messenger.java
@@ -21,4 +21,6 @@ public class Messenger {
     private String message,code;
     private  int status;
 
+
+
 }
--- src/main/java/com/toomuchcoder/api/auth/filter/AuthFilter.java
+++ src/main/java/com/toomuchcoder/api/auth/filter/AuthFilter.java
@@ -1,5 +1,18 @@
 package com.toomuchcoder.api.auth.filter;
 
+import com.toomuchcoder.api.auth.config.AuthProvider;
+import com.toomuchcoder.api.auth.exception.SecurityRuntimeException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * packageName: com.toomuchcoder.api.auth.filter
  * fileName        : AuthFilter.java
@@ -11,5 +24,31 @@ package com.toomuchcoder.api.auth.filter;
  * =============================================
  * 2022-05-25         solyikwon      최초 생성
  **/
-public class AuthFilter {
+@RequiredArgsConstructor
+public class AuthFilter extends OncePerRequestFilter {
+    private final AuthProvider provider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+       String token = provider.resolveToken(request);
+       try {
+           if (token != null && provider.validateToken(token)) {
+               Authentication auth = provider.getAuthenticate(token);
+               SecurityContextHolder.clearContext();
+               SecurityContextHolder.getContext().setAuthentication(auth);
+
+           }
+       }
+       catch (SecurityRuntimeException ex) {
+           //this is very important, since it guarantees the user is not authenticated at all
+           SecurityContextHolder.clearContext();
+           response.sendError(ex.getHttpStatus().value(), ex.getMessage());
+           return;
+       } catch (Exception e) {
+           // TODO Auto-generated catch block
+           e.printStackTrace();
+       }
+       filterChain.doFilter(request,response);
+    }
 }
+
