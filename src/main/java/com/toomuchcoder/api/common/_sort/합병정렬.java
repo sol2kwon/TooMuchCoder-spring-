c11b9758874fb343d0230d75543d4775a12290ec
sol2kwon <amorfatisole@gmail.com>
2022-06-20 20:46:34 +0900

    20220620

--- src/main/java/com/toomuchcoder/api/common/_sort/합병정렬.java
+++ src/main/java/com/toomuchcoder/api/common/_sort/합병정렬.java
@@ -19,42 +19,91 @@ public class 합병정렬 {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
-    static class Solution{
+    static class Solution {
         private int[] arr;
-        private int[] sorted;
 
-        public String toString(){return null;}
+
+        public String toString() {
+            return null;
+        }
     }
 
     @FunctionalInterface
-    private interface SolutionService{
+    private interface SolutionService {
         Solution solution(Solution a);
     }
 
+    @FunctionalInterface
+    private interface TriConsumer {
+        void accept(int[] arr, int start, int middle, int end);
+    }
 
-    class Service{
-        SolutionService f = e ->{
+    @FunctionalInterface
+    private interface Tri2Consumer{
+        void accept2(int[] arr, int start, int end);
+    }
 
-            int i = e.start;
+
+
+    class Service {
+        SolutionService f = e -> {
 
 
             return Solution.builder().build();
         };
 
-
-
-        Solution test(Solution a){return f.solution(a);}
+        Solution test(Solution a) {
+            return f.solution(a);
+        }
     }
 
+    TriConsumer c = (arr, start, middle, end) -> {
+        int[] sorted = new int[arr.length];
+        int i = start;
+        int j = middle + 1;
+        int k = end;
+        //작은 순서로 배열에 삽입
+        while (i <= middle && j <= end) {
+            sorted[k] = arr[j];
+            i++;
+        }
+        if (i>middle){
+            for (int t=j; t<=end; t++){
+                sorted[k] =arr[t];
+                k++;
+            }
+        } else {
+            //정렬된 배열 삽입
+            for (int t=i; t<=middle; t++){
+                sorted[k] = arr[t];
+                k++;
+            }
+            for (int t = start; t<=middle; t++){
+                sorted[k] = arr[t];
+                k++;
+            }
+        }
+    };
+
+    Tri2Consumer d = (arr, start, end) -> {
+      if(start<end){
+          int middle = (start+end)/2;
+          //재귀함수 불러와야하는데 람다식 안에서 가능한지 찾아보기.
+
+      }
+    };
+
     @Test
-    void testSolution(){
-        int[] arr ={29,1,10,54,11,3};
-        int[] sorted = new int [arr.length];
+    void testSolution() {
+        int[] arr = {29, 1, 10, 54, 11, 3};
+
 
         Solution g = Solution.builder().build();
         Service h = new Service();
         System.out.println(h.test(g));
     }
-
-
 }
+
+
+
+
