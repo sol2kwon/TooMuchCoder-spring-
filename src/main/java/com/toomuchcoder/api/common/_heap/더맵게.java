ece6031c3eb89f43bedee4e8ccef4b3d11c9a24f
sol2kwon <amorfatisole@gmail.com>
2022-06-25 18:01:51 +0900

    20220625

--- src/main/java/com/toomuchcoder/api/common/_heap/더맵게.java
+++ src/main/java/com/toomuchcoder/api/common/_heap/더맵게.java
@@ -1,41 +1,86 @@
 package com.toomuchcoder.api.common._heap;
 
-import com.toomuchcoder.api.common._bruteForce.ATM;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 /**
  * packageName: com.toomuchcoder.api.common._heap
  * fileName        : 더맵게.java
  * author          : solyikwon
- * date            : 2022-05-25
+ * date            : 2022-06-25
  * desc            :
  * =============================================
  * DATE              AUTHOR        NOTE
  * =============================================
- * 2022-05-25         solyikwon      최초 생성
+ * 2022-06-25         solyikwon      최초 생성
  **/
 public class 더맵게 {
     @Builder
     @Getter
     @NoArgsConstructor
+    @AllArgsConstructor
 
     public static class Solution{
+        private int [] scoville;
+        private int k;
+        private int answer;
+        private Integer[] scovilleList;
 
 
         public String toString(){
-            return String.format(" ");
+            return Arrays.toString(scovilleList);
         }
 
     }
-    @FunctionalInterface private interface ISolution{
-        ATM.Solution solution (ATM.Solution s);
+    @FunctionalInterface
+    private interface SolutionService{
+        Solution solution (Solution s);
     }
-    @Test
-    void testSolution(){
 
+    class Service {
+        SolutionService f = e -> {
+            PriorityQueue<Integer> scovilleList = new PriorityQueue<Integer>();
+            for (int i = 0; i < e.scoville.length; i++) {
+                scovilleList.offer(e.scoville[i]);
+            }
+            int answer = 0;
+            while (scovilleList.size() > 1 && scovilleList.peek() < e.k) {
+                int min1 = scovilleList.poll();
+                int min2 = scovilleList.poll();
+
+                int mix = min1 + (min2 * 2);
+                scovilleList.offer(mix);
+                 e.answer = answer++;
+                return Solution.builder().answer(e.answer).build();
+            }
+            if (scovilleList.size() <= 1 && scovilleList.peek() < e.k) {
+                 e.answer = answer = -1;
+                return Solution.builder().answer(e.answer).build();
+
+            }
+            return Solution.builder().scovilleList(e.scovilleList).build();
+
+        };
+
+        Solution test(Solution s) {
+            return f.solution(s);
+        }
     }
-}
+
+        @Test
+        void testSolution(){
+            int [] scoville = {1, 2, 3, 9, 10, 12};
+            int k = 7;
+            Service s2 = new Service();
+            Solution s = Solution.builder().scoville(scoville).k(k).build();
+            System.out.println(s2.test(s));
+
+        }
+    }
+
--- src/main/java/com/toomuchcoder/api/common/_stackQueue/프린트.java
+++ src/main/java/com/toomuchcoder/api/common/_stackQueue/프린트.java
@@ -22,7 +22,7 @@ import java.util.PriorityQueue;
  * 이 새롭게 개발한 프린터는 아래와 같은 방식으로 인쇄 작업을 수행합니다.
  * 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
  * 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
- * 그렇지 않으면 J를 인쇄합니다.
+ * 그렇지 않으면 J를 인쇄qwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww합니다.
  * 예를 들어, 4개의 문서(A, B, C, D)가 순서대로 인쇄 대기목록에 있고 중요도가 2 1 3 2 라면 C D A B 순으로 인쇄하게 됩니다.
  * 내가 인쇄를 요청한 문서가 몇 번째로 인쇄되는지 알고 싶습니다. 위의 예에서 C는 1번째로, A는 3번째로 인쇄됩니다.
  * 현재 대기목록에 있는 문서의 중요도가 순서대로 담긴 배열 priorities와
@@ -95,8 +95,8 @@ public class 프린트 {
     }
     @Test
     void testSolution(){
-        Integer[] priorities = {1, 1, 9, 1, 1, 1};
-        int location = 0;
+        Integer[] priorities = {2, 1, 3, 2};
+        int location = 2;
 
         Service s2 = new Service();
         Solution s = Solution.builder()
