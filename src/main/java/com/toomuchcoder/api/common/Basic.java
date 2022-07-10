a93e8f07e793eb7e7dedcc6a74a46bfa1e8ef7e5
sol2kwon <amorfatisole@gmail.com>
2022-07-10 16:20:12 +0900

    20220710

---
+++ src/main/java/com/toomuchcoder/api/common/Basic.java
@@ -0,0 +1,13 @@
+package com.toomuchcoder.api.common;
+
+public class Basic {
+        public static void main(String[] args) {
+            for (int i =1; i<=3; i++){
+                if (i==2){
+                    continue;
+                }
+                System.out.printf("i = %d%n",i);
+            }
+        }
+
+}//continue와 break의 차이 //지우기
